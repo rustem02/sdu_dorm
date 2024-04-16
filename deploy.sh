a4a0f7272f70befcad310d07ba5289a8fac71aff
Rustem <75470901+rustem02@users.noreply.github.com>
2024-04-16 14:30:10 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -23,7 +23,7 @@ sudo apt-get install -y python3.10 python3.10-venv # Установка паке
 # Create a virtual environment
 echo "Creating a virtual environment"
 sudo python3.10 -m venv venv
-sudo source venv/bin/activate
+source venv/bin/activate
 
 # Install application dependencies from requirements.txt
 echo "Install application dependencies from requirements.txt"
