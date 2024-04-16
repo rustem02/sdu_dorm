efde8cab60510fcc6ee82125165284869e684135
Rustem <75470901+rustem02@users.noreply.github.com>
2024-04-16 13:55:57 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,8 @@ sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
 
 echo "installing python 3.10.2 and pip"
-sudo apt-get install -y python3.10.4 python3-pip
+sudo apt-get install -y python3.10.2
+sudo apt install python3-pip
 
 # Install application dependencies from requirements.txt
 echo "Install application dependencies from requirements.txt"
